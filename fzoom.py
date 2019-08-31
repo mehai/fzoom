e9adc91ae08b73bbdd3639bcddc7434cc1957414
mehai <mihaizanfir98@gmail.com>
2019-08-31 21:46:12 +0300

    create basic click script

--- fzoom.py
+++ fzoom.py
@@ -1,8 +1,42 @@
 import click
 
-@click.command()
-def main():
-    print('my first python CLI app')
+class Config:
+	def __init__(self):
+		self.verbose = False
 
-if __name__ == '__main__':
-    main()
+config = click.make_pass_decorator(Config, ensure=True)
+
+@click.group()
+@click.option('--verbose', is_flag=True)
+@click.option('--home-directory', type=click.Path())
+@config
+def main(config, verbose, home_directory):
+	'''
+		This script analizes a text file.
+		It takes in only one argument, the path to the file on which to
+		perform the analysis. It also has multiple composing sub-commands
+		with which to specify the desired action.
+	'''
+	config.verbose = verbose
+	if home_directory is None:
+		home_directory = '.'
+	config.home_directory = home_directory
+
+
+@main.command()
+@click.option('--string', default='World', help='Who to greet')
+@click.option('--repeat', default=1, help='How many times to do it')
+@click.argument('input', type=click.File('r'), default='-', required=False)
+@config
+def say(config, string, repeat, input):
+	'''
+		This command greets somebody.
+	'''
+	if config.verbose:
+		click.echo('We are in verbose mode')
+
+	click.echo('Home directory is {config.home_directory}')
+
+	click.echo(f'file to read from is {input}')
+	for x in range(repeat):
+		click.echo(f'Hello, {string}!')
